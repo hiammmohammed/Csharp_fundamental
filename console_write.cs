f76fa5868445b7cc6398c018213efe20d122acb4
hiammmohammed <haiammohammoud@gmail.com>
2024-07-19 16:31:47 +0300

    frist

---
+++ console_write.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace csharp_fundamentals.ConsalApp
+{
+    internal class console_write
+    {
+        static void Main(String[] args)
+        {
+
+            // Console.WriteLin
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(",c# language ");
+
+            Console.WriteLine("----------------------------------------------------------------");
+            //Console.Write
+            Console.Write("Hello, World!");
+            Console.Write(",c# language \n ");
+
+            Console.WriteLine("----------------------------------------------------------------");
+            // use \n
+            Console.Write("Hello, World! \n \n \n \n ");
+            Console.WriteLine(",c# language");
+            Console.WriteLine("----------------------------------------------------------------");
+
+
+
+        }
+    }
+}
+
---
+++ console_write2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals
+{
+    internal class console_write2
+    {
+        static void Main (string[] args)
+        {
+            // put title to your console
+            Console.Title = "C# Fundamental | Passionate coder";
+            // color on text
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("hallo");
+            // color on background 
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("world");
+           
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.BackgroundColor= ConsoleColor.Black;
+            
+            //wait the key from user to end process
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
